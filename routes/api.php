43bcb50ad1e972d08b7c3e489b751ff12a54ce05
nada <nada.mohamed@extech.com>
2022-03-09 21:26:14 +0200

    create, view transaction

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\SubCategoryController;
+use App\Http\Controllers\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,10 +20,12 @@ use App\Http\Controllers\SubCategoryController;
 // public 
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/subcategories', [SubCategoryController::class, 'index']);
+Route::get('/transaction/{id}', [TransactionController::class, 'show']);
 
 // to be protected
 Route::post('/category', [CategoryController::class, 'store']);
 Route::post('/subcategory', [SubCategoryController::class, 'store']);
+Route::post('/transaction', [TransactionController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
